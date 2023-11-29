778787f18df6ba3605d2f141da3fa28fd89f9a81
gnchaithanya <chaithugajjala28@gmail.com>
2023-11-29 21:56:04 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from app.models import *
 from app.serializers import *
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 def employeedata(request):
     EMPDATA=Employee.objects.all()
     EMPJSONDATA=EmployeeModelSerializers(EMPDATA,many=True)
